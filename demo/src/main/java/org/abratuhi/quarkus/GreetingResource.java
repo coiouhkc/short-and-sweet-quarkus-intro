648fc5c4f7dd1e1cefad38ab3281ad0a0930ca2a
Alexei Bratuhin <alexei.bratuhin@googlemail.com>
2022-10-12 12:34:09 +0200

    stage 2: live reload of changes
    in endpoint and in application.properties

--- demo/src/main/java/org/abratuhi/quarkus/GreetingResource.java
+++ demo/src/main/java/org/abratuhi/quarkus/GreetingResource.java
@@ -1,5 +1,7 @@
 package org.abratuhi.quarkus;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +10,12 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "app.greeting", defaultValue = "Hello from RESTEasy Reactive")
+    String greeting;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return greeting;
     }
 }
