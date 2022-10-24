db6619442989ee7c92a74f2fbb27d0f03c7ddaad
Alexei Bratuhin <alexei.bratuhin@googlemail.com>
2022-10-24 15:00:56 +0200

    Bug-or-feature: quarkus.datasource.devservices.port is not set

--- demo/src/test/java/org/abratuhi/quarkus/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.abratuhi.quarkus;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
